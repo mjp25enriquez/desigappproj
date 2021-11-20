89f638ffb171086089cd569237c1074436912a87
mjp25enriquez <mjp25enriquez@gmail.com>
2021-11-20 07:32:16 +0000

    Committing designappproj files.

---
+++ design_app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp design_app.py tempdir/.
+cp -r static/* tempdir/static/.
+cp -r static/* tempdir/static/. 
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+
+echo "COPY  ./static /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates" >> tempdir/Dockerfile
+echo "COPY  design_app.py /home/myapp/" >> tempdir/Dockerfile
+
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/design_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+
+docker build -t designapp .
+docker run -t -d -p 5050:5050 --name designapprunning designapp
+docker ps -a
